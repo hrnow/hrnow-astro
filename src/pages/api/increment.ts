06172b6a982f38ece5b7d56b3a2b798f8dbb4493
petershaan12 <peterhiku12@gmail.com>
2025-10-03 16:19:38 +0700

    fix

--- src/pages/api/increment.ts
+++ src/pages/api/increment.ts
@@ -1,6 +1,8 @@
 import type { APIRoute } from "astro";
 import { connectDB, getDB } from "../../lib/mongodb";
 
+export const prerender = false;
+
 export const POST: APIRoute = async ({ request }) => {
   try {
     const body = await request.json();
